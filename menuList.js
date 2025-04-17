592da3d34f5999f423a47146dc732e4f1be4f078
kaustubh2020 <kaustubhjaiswal200@gamil.com>
2025-04-18 01:31:55 +0530

    New window options

--- menuList.js
+++ menuList.js
@@ -15,8 +15,13 @@ exports.menuList = [
           const win2 = new BrowserWindow({
             height: 300,
             width: 400,
+            show: false,
+            backgroundColor: "#2e2c29",
+            movable: false,
           });
           win2.loadFile("./index2.html");
+          //win2.loadURL("https://google.com");
+          win2.once("ready-to-show", () => win2.show());
         },
       },
       {
